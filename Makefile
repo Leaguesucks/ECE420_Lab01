38383f380a163d1c6f690d41b5c7562466c8e4ff
Dang Nguyen <dpn@ualberta.ca>
2025-01-10 16:24:06 -0700

    By Dang Nguyen: Update the Makefile and main.c

--- Makefile
+++ Makefile
@@ -1,10 +1,16 @@
-all: 
+all: matrixgen main
+
+main: main.o lab1_IO.o
+	gcc -Wall -pthread -g main.o lab1_IO.o -o main
+
+main.o: lab1_IO.h timer.h
+	gcc -Wall -pthread -g -c main.c -o main.o
 
 lab1_IO.o: lab1_IO.c lab1_IO.h
-	gcc -Wall -g -c lab1_IO.c
+	gcc -Wall -pthread -g -c lab1_IO.c -o lab1_IO.o
 
 matrixgen:
 	gcc matrixgen.c -o matrixgen
 
 clean:
-	rm *.o matrixgen
+	rm *.o matrixgen main data_input data_output
--- main.c
+++ main.c
@@ -1,13 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <math.h>
 #include "timer.h"
 #include "lab1_IO.h"
 
+int P, Ps; // Number of thread and its square root
+int **A, **B, **C; // Matrix A, B, and C
+int n; // Size of the matrics
+double start, end; // Start and end timing of process
+
+/* The thread function
+ *      arg: The assigned rank for the thread
+ */
+void* thr_fn(void *arg) {
+    int k = *((int*) arg); // Current rank of this thread
+    int x, y; // Current row, col of the process block
+    x = floor(k/Ps);
+    y = (k%Ps)*(k%Ps);
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (
+                (n/Ps)*x <= i && i <= (n/Ps)*(x+1)-1
+                &&
+                (n/Ps)*y <= j && j <= (n/Ps)*(y+1)-1
+            )
+            {
+                for (int r = 0; r < n-1; r++) {
+                    C[i][j] += A[i][r] * B[r][j];
+                }
+            }
+            else continue;
+        }
+    }
+
+    pthread_exit(NULL);
+    return NULL; // To make very damn sure that the thread terminate
+}
+
 int main(int argc, char *argv[]) {
-    int **A, **B, **C; // matrix A, B, and C
-    int n; // size of the matrics
-    double start, end; // start and end timing of process
 
     // used the helper function from lab1_IO.h to load input
     Lab1_loadinput(&A, &B, &n);
@@ -21,6 +53,22 @@ int main(int argc, char *argv[]) {
     GET_TIME(start);
 
     // implementation of the matrix multiplication here
+    P = n*n;
+    Ps = n;
+    pthread_t threads[P];
+
+    for (int r = 0; r < P; r++) {
+        if (pthread_create(&threads[r], NULL, thr_fn, (void*) &r) != 0) {
+            fprintf(stderr, "Cannot create thread %d!\n", r);
+        } 
+    }
+
+    // Wait for all thread to terminate
+    for (int r = 0; r < P; r++) {
+        if (pthread_join(threads[r], NULL) != 0) {
+            fprintf(stderr, "Thread %d failed to terminate!\n", r);
+        }
+    }
 
     GET_TIME(end);
 
@@ -33,8 +81,8 @@ int main(int argc, char *argv[]) {
         free(B[i]);
         free(C[i]);
     }
+
     free(A);
     free(B);
     free(C);
-
 }
